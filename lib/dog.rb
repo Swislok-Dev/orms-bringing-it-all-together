b12cf42e34fa06609fcd24da2a99e3f92da86daa
Cory M <cmarker4@gmail.com>
2021-01-15 16:56:42 -0600

    commit tree 0d18a9b862f526675b78df38917d27312105b2ac

---
+++ lib/dog.rb
@@ -0,0 +1,56 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(id = nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+
+    DB[:conn].execute(sql)
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    
+    # if self.id
+    #   self.update
+    # else
+    #   sql = <<-SQL
+    #   INSERT INTO dogs (name, breed)
+    #   VALUES (?, ?)
+    #   SQL
+  
+    #   DB[:conn].execute(sql, self.name, self.breed)
+    #   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    # end
+  end
+  
+end
